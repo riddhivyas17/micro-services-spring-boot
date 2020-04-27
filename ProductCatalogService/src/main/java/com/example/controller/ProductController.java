fd46b87f6f2ad3fd49f2047371ee18196149b42d
riddhivyas17 <55535749+riddhivyas17@users.noreply.github.com>
2020-04-27 15:06:29 +0530

    cchhg

--- ProductCatalogService/src/main/java/com/example/controller/ProductController.java
+++ ProductCatalogService/src/main/java/com/example/controller/ProductController.java
@@ -13,26 +13,30 @@ import com.example.productInfo.ProductInfo;
 import com.example.service.ProductService;
 
 @RestController
-@RequestMapping(value = "products")
+@RequestMapping(value = "/products")
 public class ProductController {
-	 private final ProductService productService;
+	
+	private final ProductService productService;
 
 	    public ProductController(ProductService productService) {
 	        this.productService = productService;
 	  }
-	    
+   
 @RequestMapping( method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 public void addProduct(@RequestBody final ProductInfo productVo) {
 	    productService.addProduct(productVo);
 	  }
+
 @RequestMapping(value = "/search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 public List<ProductInfo> getProductsByTypeAndName(@RequestParam(value = "type", required = false) final String type, @RequestParam(value = "name", required = false) final String name) {
     return productService.getProductsByTypeAndName(type, name);
       }  
+
 @RequestMapping(value = "/searchByType", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 public List<ProductInfo> getProductsByTypeAndName(@RequestParam(value = "type", required = false) final String type) {
     return productService.getProductsByType(type);
       }
+
 @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 public List<ProductInfo> getProducts() {
     return productService.getProducts();
